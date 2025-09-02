72180d3de9f0567f637b9c584a295681b079f505
thecook060 <thecook060@gmail.com>
2025-09-02 20:11:29 +0530

    feature/ login

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,13 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'login',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
 export class Login {
-  counter = 0;
+  counter = 20;
   
   increment() {
     debugger
